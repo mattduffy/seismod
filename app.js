21b69987f8ea07dac4026da83e489528a2dff879
Matthew Duffy <mattduffy@gmail.com>
2016-06-13 18:28:57 -0700

    Still don't quite know what this will be yet.

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-'use strict';
-let app = require('express')();
+var app = require('express')()
+	, bodyParser = require('body-parser')
+	;
 
-app.get('/', (req, res, next)=>{
+app.use(bodyParser.json);
+app.use(bodyParser.text({type: 'text/*'}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res, next){
 	res.send("wow");
-	return;
 });
-app.post('/', (req, res, next)=>{
-  res.JSON(req.body);
-  return;
+app.post('/', function(req, res, next){
+  console.log(req.body);
+  res.send(req.body);
 });
 
-app.listen(4000, ()=>{
-  console.log("listening on port 4000");
+app.use(function(res, req, next){
+  res.status(404).send("NOPE");
+});
+app.listen(4000, function(error){
+  if(error) {
+    console.error(error);
+  } else {
+    console.log("listening on port 4000");
+  }
 });
